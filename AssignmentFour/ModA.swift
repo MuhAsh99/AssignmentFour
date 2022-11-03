eaf475663bcc13f178a7995bae3f0ca8cc85f98b
MuhAsh99 <mjashraf@smu.edu>
2022-11-03 15:55:04 -0500

    updating
    trying to figure out why openCV won't get added properly to this

--- AssignmentFour/ModA.swift
+++ AssignmentFour/ModA.swift
@@ -9,11 +9,13 @@ import UIKit
 
 class ModA: UIViewController {
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+  
     
 
     /*
