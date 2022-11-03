be0b3f5a4e4a1e908c2aca5839e2bd219af1f72b
MuhAsh99 <mjashraf@smu.edu>
2022-11-03 15:31:27 -0500

    openCV
    added OpenCV to project

--- AssignmentFour/ViewController.swift
+++ AssignmentFour/ViewController.swift
@@ -6,8 +6,15 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    let videoManager:VideoAnalgesic! = nil
+    
+    
+    let bridge = OpenCVBridge()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
