5059e81569a547f55152ce9afaf9887776812757
MuhAsh99 <mjashraf@smu.edu>
2022-11-03 15:45:04 -0500

    Create ModA.swift
    added view controller

---
+++ AssignmentFour/ModA.swift
@@ -0,0 +1,29 @@
+//
+//  ModA.swift
+//  AssignmentFour
+//
+//  Created by Muhammad Ashraf on 11/3/22.
+//
+
+import UIKit
+
+class ModA: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
